b5d0696185d666be77f5ed1d9188a17cb1ffadae
fwipsity <fwipsity@gmail.com>
2021-04-28 21:15:38 +0300

    plugin core finished

--- spamBadge/plugin.js
+++ spamBadge/plugin.js
@@ -1,1 +1,16 @@
-console.log("plugin loaded")
+socket.on("message", (user, message) => {
+    let lastMessage = messages.children[messages.children.length - 2];
+    if (lastMessage.innerText = `${user}: ${message}`) {
+        messages.remove(messages.children[messages.children.length - 1]);
+
+        if (lastMessage.children.length === 2 && lastMessage.children[1].classList.value === "badge bg-secondary") {
+            lastMessage.children[1].innerText = parseInt(lastMessage.children[1].innerText) + 1;
+        } else {
+            let badge = document.createElement("span");
+            badge.classList.value = "badge bg-secondary";
+            badge.innerText = "2";
+            lastMessage.appendChild(badge);
+        }
+    }
+
+});
