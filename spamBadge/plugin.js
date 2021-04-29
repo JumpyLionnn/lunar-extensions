76d92d0cfc6b87f38cbbc5b5436f2cf85165405e
fwipsity <fwipsity@gmail.com>
2021-04-29 19:46:57 +0300

    removed debug and added some more style

--- spamBadge/plugin.js
+++ spamBadge/plugin.js
@@ -1,19 +1,19 @@
+const badgeClassNames = "badge bg-secondary ms-1";
+
 socket.on("message", (user, message) => {
     if (messages.length === 1) {
         return;
     }
     let lastMessage = messages.children[messages.children.length - 2];
 
-    if (((lastMessage.children[0].classList.value === "badge bg-secondary" && lastMessage.innerText.startsWith(lastMessage.children[0].innerText + user)) ||
+    if (((lastMessage.children[0].classList.value === badgeClassNames && lastMessage.innerText.startsWith(lastMessage.children[0].innerText + user)) ||
             (lastMessage.innerText.startsWith(user))) && lastMessage.id === `-${message}-`) {
         messages.removeChild(messages.children[messages.children.length - 1]);
-        console.log(lastMessage.children[0].classList);
-        console.log(lastMessage.children.length);
-        if (lastMessage.children.length === 2 && lastMessage.children[0].classList.value === "badge bg-secondary") {
+        if (lastMessage.children.length === 2 && lastMessage.children[0].classList.value === badgeClassNames) {
             lastMessage.children[0].innerText = parseInt(lastMessage.children[0].innerText) + 1;
         } else {
             let badge = document.createElement("span");
-            badge.classList.value = "badge bg-secondary";
+            badge.classList.value = badgeClassNames;
             badge.innerText = "2";
             lastMessage.prepend(badge);
         }
