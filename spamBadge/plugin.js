974945cdd19af544588e9208c986b0ff60468c66
fwipsity <fwipsity@gmail.com>
2021-05-06 08:04:48 +0300

    fixed a bug

--- spamBadge/plugin.js
+++ spamBadge/plugin.js
@@ -6,7 +6,11 @@ socket.on("message", (user, message) => {
     }
     let lastMessage = messages.children[messages.children.length - 2];
     let lastText = lastMessage.id;
-    let lastMessageUser = lastMessage.querySelector("#messageUsername").innerText;
+    let lastMessageUserElement = lastMessage.querySelector("#messageUsername");
+    if (lastMessageUserElement === null) {
+        return;
+    }
+    let lastMessageUser = lastMessageUserElement.innerText;
     let lastMessageBadge = lastMessage.querySelector("#spamBadge");
     if (lastText === `-${message}-` && user === lastMessageUser) {
         messages.removeChild(messages.children[messages.children.length - 1]);
